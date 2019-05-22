f615ac0c3c1b7555d745fcc6bac0566ee5eb9fdb
ANASTASIA <anastasia@ANASTASIAs-MacBook-Pro.local>
2019-05-22 16:21:47 +0300

    what is gong on

--- UnitQuorum/Controller/LoginViewController.swift
+++ UnitQuorum/Controller/LoginViewController.swift
@@ -12,4 +12,20 @@ import UIKit
 class LoginViewController: UIViewController {
     
     
+    @IBOutlet weak var xloginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var plainLoginButton: UIButton!
+    @IBOutlet weak var webLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+ 
+
 }
