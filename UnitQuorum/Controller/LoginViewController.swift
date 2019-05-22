c5bb9302f696c9455c2b1dd985ebc49416d56156
ANASTASIA <anastasia@ANASTASIAs-MacBook-Pro.local>
2019-05-22 14:40:17 +0300

    added first page uiselements

---
+++ UnitQuorum/Controller/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  UnitQuorum
+//
+//  Created by ANASTASIA on 5/22/19.
+//  Copyright © 2019 Anastasiia ALOKHINA. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+}
